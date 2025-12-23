05843aed3bae5f88cf6f7ff59e0ce4a5dbe59b1d
PythonDeveloper2006 <mypcnout@gmail.com>
2025-12-23 23:24:09 +0100

    2

--- client.py
+++ client.py
@@ -3,23 +3,23 @@
 # url = "https://whale-app-t34ml.ondigitalocean.app/вот тут что ли надо каждый раз писать свое сообщение? разве это так делается?"
 # print(urllib.request.urlopen(url).read().decode())
 
+if __name__ == "__main__":
+    import asyncio, websockets
 
-import asyncio, websockets
+    async def reader(ws):
+        async for msg in ws:
+            print("\n<", msg)
 
-async def reader(ws):
-    async for msg in ws:
-        print("\n<", msg)
+    async def writer(ws):
+        while True:
+            msg = await asyncio.to_thread(input, "> ")
+            await ws.send(msg)
 
-async def writer(ws):
-    while True:
-        msg = await asyncio.to_thread(input, "> ")
-        await ws.send(msg)
+    async def main():
+        async with websockets.connect("ws://127.0.0.1:9000") as ws:
+            await asyncio.gather(
+                reader(ws),
+                writer(ws),
+            )
 
-async def main():
-    async with websockets.connect("ws://127.0.0.1:9000") as ws:
-        await asyncio.gather(
-            reader(ws),
-            writer(ws),
-        )
-
-asyncio.run(main())
+    asyncio.run(main())
--- server.py
+++ server.py
@@ -1,46 +1,48 @@
-# import os
-# import http.server
-# import socketserver
-#
-# from http import HTTPStatus
-#
-#
-# class Handler(http.server.SimpleHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(HTTPStatus.OK)
-#         self.end_headers()
-#         msg = 'Ты написал %s' % (self.path)
-#         self.wfile.write(msg.encode())
-#
-#
-# port = int(os.getenv('PORT', 80))
-# print('Listening on port %s' % (port))
-# httpd = socketserver.TCPServer(('', port), Handler)
-# httpd.serve_forever()
+if __name__ == "__main__":
+
+    # import http.server
+    # import socketserver
+    #
+    # from http import HTTPStatus
+    #
+    #
+    # class Handler(http.server.SimpleHTTPRequestHandler):
+    #     def do_GET(self):
+    #         self.send_response(HTTPStatus.OK)
+    #         self.end_headers()
+    #         msg = 'Ты написал %s' % (self.path)
+    #         self.wfile.write(msg.encode())
+    #
+    #
+    # port = int(os.getenv('PORT', 80))
+    # print('Listening on port %s' % (port))
+    # httpd = socketserver.TCPServer(('', port), Handler)
+    # httpd.serve_forever()
 
 
-import asyncio, websockets
+    import asyncio, websockets, os
 
-clients = set()
+    PORT = int(os.getenv('PORT', 80))
+    clients = set()
 
-async def handler(ws):
-    print("connect")
-    clients.add(ws)
-    try:
-        async for msg in ws:
-            print("recv:", msg)
-            await asyncio.gather(
-                *(c.send(msg) for c in clients if c != ws)
-            )
-    except Exception as e:
-        print("error:", e)
-    finally:
-        clients.remove(ws)
-        print("disconnect")
+    async def handler(ws):
+        print("connect")
+        clients.add(ws)
+        try:
+            async for msg in ws:
+                print("recv:", msg)
+                await asyncio.gather(
+                    *(c.send(msg) for c in clients if c != ws)
+                )
+        except Exception as e:
+            print("error:", e)
+        finally:
+            clients.remove(ws)
+            print("disconnect")
 
-async def main():
-    print("server start:9000")
-    async with websockets.serve(handler, "0.0.0.0", 9000):
-        await asyncio.Future()
+    async def main():
+        print("server start:9000")
+        async with websockets.serve(handler, "0.0.0.0", PORT):
+            await asyncio.Future()
 
-asyncio.run(main())
+    asyncio.run(main())
